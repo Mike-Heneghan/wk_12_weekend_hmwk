a3431e36a93a6a99d292eba49c5a4faf1813cbd2
Mike-Heneghan <m.heneghan@live.co.uk>
2018-05-28 08:52:22 +0100

    Added languages unordered list for each country.

--- src/views/country_detail_view.js
+++ src/views/country_detail_view.js
@@ -11,6 +11,18 @@ CountryDetailView.prototype.createDetailView = function () {
   countryHeading.textContent = this.country.name;
   countryDetail.appendChild(countryHeading);
 
+  const countryLanguages = document.createElement('ul');
+  const languagesArray = [];
+  this.country.languages.forEach((language) => {
+    languagesArray.push(language.name);
+  });
+  languagesArray.forEach((language) => {
+    const languageItem = document.createElement('li');
+    languageItem.textContent = language;
+    countryLanguages.appendChild(languageItem);
+  });
+  countryDetail.appendChild(countryLanguages);
+
   return countryDetail;
 
 };
